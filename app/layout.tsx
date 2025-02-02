e4b07280b36b2446670fbf53a4871ca13ca77708
Omar Abdesslem <omarabdesslem@Omars-MacBook-Air.local>
2025-02-02 17:11:08 +0100

    Changed Title name

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Portfolio: Omar Abdesslem",
+  title: "Portfolio",
   description: "Portfolio of Omar Abdesslem",
 };
 
