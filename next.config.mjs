9e89efd1f6061698d783cda769f8c4391522d834
Omar <omar.abdesslem@etu.unige.ch>
2025-06-08 14:07:21 +0100

    redirect utm pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'query',
+            key: 'utm_medium',
+          },
+        ],
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'query',
+            key: 'utm_campaign',
+          },
+        ],
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
