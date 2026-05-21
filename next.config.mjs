2fcab675a1955472be59f35b17711eb4afa15d64
Omar Abdesslem <102889312+omarabdesslem@users.noreply.github.com>
2026-05-21 15:31:15 +0200

    Refactor redirects based on GitHub Pages environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,42 @@
 /** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'query',
-            key: 'utm_medium',
-          },
-        ],
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'query',
-            key: 'utm_campaign',
-          },
-        ],
-        destination: '/',
-        permanent: true,
-      },
-    ];
-  },
+  ...(isGitHubPages
+    ? {
+        output: 'export',
+        images: {
+          unoptimized: true,
+        },
+      }
+    : {
+        async redirects() {
+          return [
+            {
+              source: '/:path*',
+              has: [
+                {
+                  type: 'query',
+                  key: 'utm_medium',
+                },
+              ],
+              destination: '/',
+              permanent: true,
+            },
+            {
+              source: '/:path*',
+              has: [
+                {
+                  type: 'query',
+                  key: 'utm_campaign',
+                },
+              ],
+              destination: '/',
+              permanent: true,
+            },
+          ];
+        },
+      }),
 };
 
 export default nextConfig;
