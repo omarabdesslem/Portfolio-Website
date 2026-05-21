835526b8edd6a1741801ab53bafbe732a30301b1
Omar <omar.abdesslem@etu.unige.ch>
2026-05-21 15:47:10 +0200

    Revert "Refactor redirects based on GitHub Pages environment"
    This reverts commit 2fcab675a1955472be59f35b17711eb4afa15d64.

--- next.config.mjs
+++ next.config.mjs
@@ -1,42 +1,31 @@
 /** @type {import('next').NextConfig} */
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-
 const nextConfig = {
-  ...(isGitHubPages
-    ? {
-        output: 'export',
-        images: {
-          unoptimized: true,
-        },
-      }
-    : {
-        async redirects() {
-          return [
-            {
-              source: '/:path*',
-              has: [
-                {
-                  type: 'query',
-                  key: 'utm_medium',
-                },
-              ],
-              destination: '/',
-              permanent: true,
-            },
-            {
-              source: '/:path*',
-              has: [
-                {
-                  type: 'query',
-                  key: 'utm_campaign',
-                },
-              ],
-              destination: '/',
-              permanent: true,
-            },
-          ];
-        },
-      }),
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'query',
+            key: 'utm_medium',
+          },
+        ],
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'query',
+            key: 'utm_campaign',
+          },
+        ],
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
